363f04f19e83fa9558c9c4dbe65154da07cd3ae1
Sarang Rawat <55296836+Git-Sarang@users.noreply.github.com>
2023-07-27 17:43:22 +0530

    update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 pip install --upgrade
-pip install -r requirements.txt
+#pip install -r requirements.txt
+pip install pandas
+pip install streamlit
+pip install pickle
+pip install requests
 streamlit run app.py
