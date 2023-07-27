762e39fff9b34b91269a374b3e60a8ec08c7ca1f
Sarang Rawat <55296836+Git-Sarang@users.noreply.github.com>
2023-07-27 17:59:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 pip install --upgrade
 /opt/render/project/src/.venv/bin/python3.7 -m pip install --upgrade pip
-#pip install -r requirements.txt
+
 pip install pandas
 pip install streamlit
 pip install pickle
